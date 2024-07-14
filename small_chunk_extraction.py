d863b3f69bff38d0fb053b9143c6116f20a3cd83
Ridansh Kaul <130085006+Ridansh71@users.noreply.github.com>
2024-07-14 06:25:36 +0530

    Update small_chunk_extraction.py

--- small_chunk_extraction.py
+++ small_chunk_extraction.py
@@ -1,1 +1,9 @@
+### Case-2 Small chunk Extraction
 
+def extract_data_in_chunks(file_path, chunk_size=1000):
+    data = pd.read_csv(file_path, chunksize=chunk_size)
+    results = []
+    for chunk in data:
+        results.append(chunk)
+    
+    return pd.concat(results)
